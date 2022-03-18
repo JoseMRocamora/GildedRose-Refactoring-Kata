182f893637a7cec0e8bbceb07e691088c445576e
DESKTOP-0Q0TKMO\Jose <kblman@gmail.com>
2022-03-18 18:16:37 +0100

    Replace conditionals with polymorphism/interfaces. Create products hierarchy

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,9 +1,13 @@
 package com.gildedrose;
 
-import com.gildedrose.products.AbstractProduct;
-import com.gildedrose.products.GenericProduct;
+import com.gildedrose.products.*;
 
 class GildedRose {
+
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -12,7 +16,19 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            AbstractProduct product = new GenericProduct(item);
+            Product product;
+            if (AGED_BRIE.equals(item.name)) {
+                product = new AgedBrieProduct(item);
+            }
+            else if (BACKSTAGE_PASSES.equals(item.name)) {
+                product = new BackStagePasseProduct(item);
+            }
+            else if (SULFURAS_HAND_OF_RAGNAROS.equals(item.name)) {
+                product = new SulfurasProduct(item);
+            }
+            else {
+                product = new GenericProduct(item);
+            }
 
             product.updateQuality();
 
