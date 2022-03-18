1b60cb6f559e96262322c5b9a4190b313871067e
DESKTOP-0Q0TKMO\Jose <kblman@gmail.com>
2022-03-18 18:43:46 +0100

    Clear Unnecessary code for Sulfuras

--- Java/src/main/java/com/gildedrose/products/SulfurasProduct.java
+++ Java/src/main/java/com/gildedrose/products/SulfurasProduct.java
@@ -2,66 +2,17 @@ package com.gildedrose.products;
 
 import com.gildedrose.Item;
 
-public class SulfurasProduct extends AbstractProduct{
+public class SulfurasProduct extends AbstractProduct {
 
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
 
     public SulfurasProduct(Item item) {
         super(item);
+        this.name = SULFURAS_HAND_OF_RAGNAROS;
     }
 
     @Override
     public void updateQuality() {
-        if (name.equals(AGED_BRIE)) {
-            incrementQuality();
-        } else {
-            if (name.equals(BACKSTAGE_PASSES)) {
-                if (quality < 50) {
-                    quality = quality + 1;
-
-                    if (sellIn < 11) {
-                        incrementQuality();
-                    }
-
-                    if (sellIn < 6) {
-                        incrementQuality();
-                    }
-                }
-            } else {
-                if (quality > 0) {
-                    if (!name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                        quality = quality - 1;
-                    }
-                }
-            }
-        }
-
-        if (!name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-            sellIn = sellIn - 1;
-        }
-
-        if (sellIn < 0) {
-            if (name.equals(AGED_BRIE)) {
-                incrementQuality();
-            } else {
-                if (name.equals(BACKSTAGE_PASSES)) {
-                    quality = quality - quality;
-                } else {
-                    if (quality > 0) {
-                        if (!name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                            quality = quality - 1;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private void incrementQuality() {
-        if (quality < 50) {
-            quality = quality + 1;
-        }
+        // nothing to do
     }
 }
