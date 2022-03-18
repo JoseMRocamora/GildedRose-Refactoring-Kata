72e5f2a355e57cb0cf536d8b3dc08ad783f6ca92
DESKTOP-0Q0TKMO\Jose <kblman@gmail.com>
2022-03-18 18:36:28 +0100

    Clear Unnecessary code for Backstage Passes

--- Java/src/main/java/com/gildedrose/products/BackStagePasseProduct.java
+++ Java/src/main/java/com/gildedrose/products/BackStagePasseProduct.java
@@ -4,58 +4,31 @@ import com.gildedrose.Item;
 
 public class BackStagePasseProduct extends AbstractProduct{
 
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
 
     public BackStagePasseProduct(Item item) {
         super(item);
+        this.name = BACKSTAGE_PASSES;
     }
 
     @Override
     public void updateQuality() {
-        if (name.equals(AGED_BRIE)) {
-            incrementQuality();
-        } else {
-            if (name.equals(BACKSTAGE_PASSES)) {
-                if (quality < 50) {
-                    quality = quality + 1;
+        if (quality < 50) {
+            quality = quality + 1;
 
-                    if (sellIn < 11) {
-                        incrementQuality();
-                    }
+            if (sellIn < 11) {
+                incrementQuality();
+            }
 
-                    if (sellIn < 6) {
-                        incrementQuality();
-                    }
-                }
-            } else {
-                if (quality > 0) {
-                    if (!name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                        quality = quality - 1;
-                    }
-                }
+            if (sellIn < 6) {
+                incrementQuality();
             }
         }
 
-        if (!name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-            sellIn = sellIn - 1;
-        }
+        sellIn = sellIn - 1;
 
         if (sellIn < 0) {
-            if (name.equals(AGED_BRIE)) {
-                incrementQuality();
-            } else {
-                if (name.equals(BACKSTAGE_PASSES)) {
-                    quality = quality - quality;
-                } else {
-                    if (quality > 0) {
-                        if (!name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                            quality = quality - 1;
-                        }
-                    }
-                }
-            }
+            quality = 0;
         }
     }
 
